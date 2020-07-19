4fcdd596f59aa3f200ba1b21f354db8f73baf20e
M R Mizan <mrmizanbd93@gmail.com>
2020-07-19 17:25:03 +0600

    vue inter face added

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,95 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+   <div id="main-app" class="container">
+       <div class="row justify-content-center">
+              
+         <add-appointment @add='addItem'></add-appointment>
+         <search-appintment @searchRecords='SearchAppointments' :myKey="filterKey"
+         :myDir="filterDir"
+         @requestKey="changeKey"
+         @requestDir="changeDir"
+         ></search-appintment>
+         <appointment-list :appointments="filterApts" @remove="removeItem" @edit="editItem" ></appointment-list>
+      </div>
+   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import SearchAppintment from './components/SearchAppointments.vue'
+import AddAppointment from './components/AddAppointment.vue'
+import AppointmentList from './components/AppointmentList.vue'
+import axios from 'axios'
+import _ from 'lodash'
 export default {
-  name: 'App',
+  name: 'MainApp',
+  data() {
+    return {
+    filterKey:'petName',
+    filterDir:'asc',
+      appointments:[],
+      searchTerms:'',
+      aptIndex:0
+    }
+  },
   components: {
-    HelloWorld
-  }
+   SearchAppintment,
+    AddAppointment,
+     AppointmentList,
+  },
+  mounted(){
+  axios.get('./data/appointments.json')
+  .then(resposne=>(this.appointments=resposne.data.map(item=>{
+    item.aptId=this.aptIndex
+    this.aptIndex++
+    return item
+  })))
+  },
+  computed: {
+    searchedapts(){
+      return  this.appointments.filter(item=>{
+        return(
+          item.petName.toLowerCase().match(this.searchTerms.toLowerCase()) ||
+          item.petOwner.toLowerCase().match(this.searchTerms.toLowerCase()) ||
+          item.aptNotes.toLowerCase().match(this.searchTerms.toLowerCase())
+        )
+      })
+    },
+    filterApts(){
+      return _.orderBy(
+        this.searchedapts,
+        item=>{
+          return item[this.filterKey].toLowerCase()
+        },this.filterDir
+      )
+    }
+  },
+  methods: {
+    changeKey(value){
+this.filterKey=value
+    },
+    changeDir(value){
+this.filterDir=value
+    },
+    SearchAppointments(terms){
+      this.searchTerms=terms
+        
+    },
+     addItem(apt){
+        apt.aptId=this.aptIndex
+        this.aptIndex++
+        this.appointments.push(apt)
+    },
+    removeItem(apt){
+      this.appointments= _.without(this.appointments,apt);
+    },
+    editItem(id,field,text){
+      const aptIndex=_.findIndex(this.appointments,{
+        aptId:id
+      })
+      this.appointments[aptIndex][field]=text
+    },
+   
+  },
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
---
+++ src/components/AppointmentList.vue
@@ -0,0 +1,47 @@
+<template>
+   
+      <div class="col-12 col-md-10 col-lg-7">
+        <div class="list-group list-group-flush">
+          <div class="list-group-item d-flex align-items-start" v-for="item in appointments" :key="item.aptIndex">
+            <button type="button" class=" mr-2 btn-sm btn btn-danger" @click="$emit('remove',item)"><font-awesome-icon icon="trash"/></button>
+            <div class="w-100">
+              <div class="d-flex justify-content-between">
+                <span class="h4 text-primary"
+                contenteditable="coteteditable"
+                @blur="$emit('edit',item.aptId,'petName',$event.target.innerText)">{{ item.petName }}</span>
+                <span class="float-right">{{ formatedDate(item.aptDate) }}</span>
+             </div>
+                <div class="owner-name">
+                <span class=" font-weight-bold text-primary mr-1">Owner:</span>
+                <span
+                 contenteditable="coteteditable"
+                @blur="$emit('edit',item.aptId,'petOwner',$event.target.innerText)"
+                >{{ item.petOwner }}</span>
+                    </div>
+                  
+                <div contenteditable="coteteditable"
+                @blur="$emit('edit',item.aptId,'aptNotes',$event.target.innerText)">
+                      {{ item.aptNotes }}
+            </div>
+           </div>
+         </div>
+       </div>
+    </div>
+</template>
+<script>
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
+import moment from 'moment'
+export default {
+    name:'AppointmentList',
+    props:["appointments"],
+    components:{
+        FontAwesomeIcon,
+    },
+    methods: {
+        formatedDate(date){
+            return moment(new Date(date)).format("DD-MM-YY, hh:mm a")
+        }
+    },
+
+}
+</script>
